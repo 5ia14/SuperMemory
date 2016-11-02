3d8abc725ea1b775583ecb73bcc01f52bba7bb68
5ia 14 <pcdokus@gmail.com>
2016-11-02 14:32:20 +0100

    linked buttons

--- app/src/main/java/com/example/admin/supermemory/MainActivity.java
+++ app/src/main/java/com/example/admin/supermemory/MainActivity.java
@@ -28,10 +28,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showSinglePlayer(View view) {
-        setContentView(R.layout.single_player);
+        setContentView(R.layout.play);
     }
 
     public void showMain(View view) {
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.multiplay);
     }
 }
