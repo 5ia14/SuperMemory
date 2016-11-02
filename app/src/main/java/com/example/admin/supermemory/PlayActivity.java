b52d50915456d97609fe54158582071de01e55b3
sadrir <sadri.rammal@hotmail.com>
2016-11-02 13:47:37 +0100

    Created Play Screen

--- app/src/main/java/com/example/admin/supermemory/PlayActivity.java
+++ app/src/main/java/com/example/admin/supermemory/PlayActivity.java
@@ -1,9 +1,17 @@
 package com.example.admin.supermemory;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Sadri on 02.11.2016.
  */
-public class PlayActivity {
+public class PlayActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 }
