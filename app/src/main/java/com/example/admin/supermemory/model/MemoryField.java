43ab36c9df5bc71e415c052704e6dfbe145ac0e9
sadrir <sadri.rammal@hotmail.com>
2016-11-02 15:47:58 +0100

    Created Play Screen Created MultiPlayScreen

--- app/src/main/java/com/example/admin/supermemory/model/MemoryField.java
+++ app/src/main/java/com/example/admin/supermemory/model/MemoryField.java
@@ -13,7 +13,16 @@ public class MemoryField {
     }
 
     public void setCards(){
+        Card[] cardsL = new Card[15];
+        for(int i = 0; i < 14; i++){
+            //cardsL = new Card(i, android.R.drawable.);
+        }
 
+        for(int a = 0; a < 5; a++){
+            for(int b = 0; b < 4; b++){
+                //cards[a][b] = new Card(
+            }
+        }
         //setzt 15 random bilder auf 30 karten und setzt noch zu jeder card eine value
     }
 }
