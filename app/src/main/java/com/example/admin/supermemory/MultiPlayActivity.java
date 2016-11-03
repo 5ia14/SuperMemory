2b90232241f295eb743aaa6d07ddc4ef4121d2c5
5ia 14 <pcdokus@gmail.com>
2016-11-03 14:31:01 +0100

    swag

--- app/src/main/java/com/example/admin/supermemory/MultiPlayActivity.java
+++ app/src/main/java/com/example/admin/supermemory/MultiPlayActivity.java
@@ -1,15 +1,18 @@
 package com.example.admin.supermemory;
 
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.media.Image;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.drawable.DrawableWrapper;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,11 +23,47 @@ import java.util.Random;
  * Created by Sadri on 02.11.2016.
  */
 public class MultiPlayActivity extends AppCompatActivity {
+    /* put this into your activity class */
+    private SensorManager mSensorManager;
+    private float mAccel; // acceleration apart from gravity
+    private float mAccelCurrent; // current acceleration including gravity
+    private float mAccelLast; // last acceleration including gravity
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+        public void onSensorChanged(SensorEvent se) {
+            float x = se.values[0];
+            float y = se.values[1];
+            float z = se.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+
+            if (mAccel > 20) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Shuffle", Toast.LENGTH_SHORT);
+                toast.show();
+
+                shuffleCards();
+
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(400);
+            }
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
 
     private List<ImageButton> ib = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multi_player);
 
@@ -36,7 +75,19 @@ public class MultiPlayActivity extends AppCompatActivity {
             ib.add((ImageButton) nextChild);
         }
 
-        shuffleCards();
+        // shuffleCards();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mSensorListener);
+        super.onPause();
     }
 
     private void shuffleCards() {
@@ -47,12 +98,10 @@ public class MultiPlayActivity extends AppCompatActivity {
         linearLayout.removeAllViews();
 
         for (ImageButton b : ib) {
-            b.animate().rotationX(50);
-
             Random rnd = new Random();
-            int r = rnd.nextInt(300);
+            int r = rnd.nextInt(200);
 
-            b.setRotationY(r);
+            b.animate().rotationY(r);
             linearLayout.addView(b);
         }
     }
@@ -71,4 +120,5 @@ public class MultiPlayActivity extends AppCompatActivity {
         // shuffleCards();
         view.animate().rotationX(180).rotationY(180);
     }
+
 }
