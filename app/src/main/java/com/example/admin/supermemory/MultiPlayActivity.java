1e5a7bb4a1fb73aed759395be897b98ea6b03538
5ia 14 <pcdokus@gmail.com>
2016-11-03 14:03:24 +0100

    swag

--- app/src/main/java/com/example/admin/supermemory/MultiPlayActivity.java
+++ app/src/main/java/com/example/admin/supermemory/MultiPlayActivity.java
@@ -1,28 +1,60 @@
 package com.example.admin.supermemory;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.drawable.DrawableWrapper;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Sadri on 02.11.2016.
  */
 public class MultiPlayActivity extends AppCompatActivity {
 
+    private List<ImageButton> ib = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multi_player);
 
-        /*ViewGroup linearLayout = (ViewGroup) findViewById(R.id.memoryField);
+        ViewGroup linearLayout = (ViewGroup) findViewById(R.id.memoryField);
         assert linearLayout != null;
 
         for (int index = 0; index < linearLayout.getChildCount(); ++index) {
             View nextChild = linearLayout.getChildAt(index);
-            nextChild.animate().rotationX(180).rotationY(180);
-        }*/
+            ib.add((ImageButton) nextChild);
+        }
+
+        shuffleCards();
+    }
+
+    private void shuffleCards() {
+        Collections.shuffle(ib);
+
+        ViewGroup linearLayout = (ViewGroup) findViewById(R.id.memoryField);
+        assert linearLayout != null;
+        linearLayout.removeAllViews();
+
+        for (ImageButton b : ib) {
+            b.animate().rotationX(50);
+
+            Random rnd = new Random();
+            int r = rnd.nextInt(300);
+
+            b.setRotationY(r);
+            linearLayout.addView(b);
+        }
     }
 
     public void showMain(View view) {
@@ -36,9 +68,7 @@ public class MultiPlayActivity extends AppCompatActivity {
     }
 
     public void handleCardClick(View view) {
+        // shuffleCards();
         view.animate().rotationX(180).rotationY(180);
     }
-
-
-
 }
